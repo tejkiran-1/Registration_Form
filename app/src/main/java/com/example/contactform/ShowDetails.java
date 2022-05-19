077dc78505415eee0320f31412c7a81ef247493a
Tejkiran Kushwaha <tej.kush1@gmail.com>
2022-05-19 21:51:09 +0530

    fINAL

--- app/src/main/java/com/example/contactform/ShowDetails.java
+++ app/src/main/java/com/example/contactform/ShowDetails.java
@@ -3,10 +3,13 @@ package com.example.contactform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ShowDetails extends AppCompatActivity {
     TextView name, email, dob;
+    Button clearBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +18,19 @@ public class ShowDetails extends AppCompatActivity {
         name = findViewById(R.id.uName);
         email = findViewById(R.id.uuEmail);
         dob = findViewById(R.id.uDOB);
+        clearBtn = findViewById(R.id.clearButton);
 
         name.setText(getIntent().getExtras().getString("name"));
         email.setText(getIntent().getExtras().getString("email"));
         dob.setText(getIntent().getExtras().getString("dob"));
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name.setText("");
+                email.setText("");
+                dob.setText("");
+            }
+        });
     }
 }
